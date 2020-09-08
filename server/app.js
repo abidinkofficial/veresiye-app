cd62545981abc21eda9fbeec0824464a94cd6973
Abidin K <abidinkofficial@gmail.com>
2020-09-08 11:32:05 +0300

    Server and DB are implemented

---
+++ server/app.js
@@ -0,0 +1,34 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+
+//Connecting to DB
+
+mongoose.connect(process.env.DB_URL, { useNewUrlParser: true, useUnifiedTopology: true });
+const db = mongoose.connection;
+db.on('error', (error) => console.error(error));
+db.once('open', () => console.log('Connected to DB! yeeyy'))
+
+//Adding some middleware
+
+app.use(express.json());
+
+app.use((req, res, next) => {
+    res.append('Access-Control-Allow-Origin', ['*']);
+    res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.append('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+});
+
+//Setting the routes
+
+app.get('/api', (req, res) => {
+    res.json({ "message": "API is working" })
+});
+
+//Setting the PORT and starting the server
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server started at PORT: ${PORT}`));
